75b01e59348b44c7fe1ab8b5d0d7f024ce67e557
George <qiaozhi_china@163.com>
2022-12-16 17:46:16 +0800

    Update Repository

--- deploy.bat
+++ deploy.bat
@@ -3,7 +3,7 @@ git add .
 echo 提交修改
 git commit -a -m "Update Repository"
 echo 拉取远程仓库到本地
-git pull origin master --allow-unrelated-histories
+git pull origin main --allow-unrelated-histories
 echo 推送到远程仓库
-git push -u origin master
+git push -u origin main
 pause
